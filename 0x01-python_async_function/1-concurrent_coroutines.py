541edec751bf9be0f54371a614081a69f987bc58
Juan Bocanegra <jbocane6@gmail.com>
2022-04-25 23:25:20 -0500

    [UPDATE ] file 1 v.2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -13,6 +13,11 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list:
+    """
+    Spawn wait_random n times with the specified max_delay.
+    wait_n should return the list of all the delays (float values).
+    The list of the delays should be in ascending order
+    """
     list_of_delays = []
     delays_ascending = []
 
