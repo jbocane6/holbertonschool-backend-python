e92920a1beacbf1461f26df37c1b346a7759fbd4
Juan Bocanegra <jbocane6@gmail.com>
2022-05-16 22:24:25 -0500

    [UPDATE] file test_utils v.3 task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -11,7 +11,8 @@ Decorate the method with @parameterized.expand
 to test the function for following inputs:
 """
 import unittest
-from utils import access_nested_map
+from unittest.mock import patch
+from utils import access_nested_map, get_json
 from parameterized import parameterized
 
 
@@ -39,6 +40,33 @@ class TestAccessNestedMap(unittest.TestCase):
         Use the assertRaises context manager to test that a KeyError is raised.
         """
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Define the TestGetJson(unittest.TestCase) class
+    and implement the TestGetJson.test_get_json method to test
+    that utils.get_json returns the expected result.
+    We don’t want to make any actual external HTTP calls.
+    Use unittest.mock.patch to patch requests.get.
+    Make sure it returns a Mock object with a json method
+    that returns test_payload which you parametrize alongside
+    the test_url that you will pass to get_json with the following inputs:
+        test_url="http://example.com", test_payload={"payload": True}
+        test_url="http://holberton.io", test_payload={"payload": False}
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        Test that utils.get_json returns the expected result.
+        """
+        with patch('requests.get') as mock:
+            mock.return_value.json.return_value = test_payload
+            self.assertEqual(get_json(test_url), test_payload)
+            mock.assert_called_once()
 
 
 if __name__ == '__main__':
